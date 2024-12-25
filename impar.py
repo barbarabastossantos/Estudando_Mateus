60a12237f37224daa629bfe99440b53d94329e61
Barbara Bastos <barbara.bastos1995@gmail.com>
2024-12-25 16:59:59 -0300

    Merge remote-tracking branch 'origin/main' impar ou par

---
+++ impar.py
@@ -0,0 +1,8 @@
+print('=' *30)
+print('              PAR OU ÍMPAR ?')
+print('=' *30)
+n=int(input('Digite um numero  inteiro : '))
+if  n % 2==0:
+    print(f' O numero {n} é par.')
+else:
+    print(f' o numero {n} é impar')
